df40c3a826772b064bd4a1f7fe80be9cf948190a
Mattias Forsman <forsman1991@gmail.com>
2018-11-28 15:06:48 +0100

    Moved controllers from mvc to msp

--- src/controller/admin-controller.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Webbmaffian\MVC\Controller;
-
-abstract class Admin_Controller extends Controller {
-	// Override functions from Controller only on client side
-}
--- src/controller/client-controller.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Webbmaffian\MVC\Controller;
-
-abstract class Client_Controller extends Controller {
-	// Override functions from Controller only on admin side
-}
