5f2c4569c04cd6a87d72d4cdfd77fc2d3caa5d51
Mattias Forsman <forsman1991@gmail.com>
2019-01-22 11:57:28 +0100

    Created template class

---
+++ src/template.php
@@ -0,0 +1,44 @@
+<?php
+	namespace Webbmaffian\Portation;
+
+	class Template extends Export {
+		public function __construct($collection)  {
+			if(!is_array($collection)) {
+				throw new Problem('Expected an array of columns.');
+			}
+
+			$this->collection = $collection;
+		}
+
+
+		private function get_spreadsheet($args = array()) {
+			$args = Helper::default_args($args, array(
+				'author' => null,
+				'title' => 'ImportTemplate',
+				'filetype' => 'xlsx',
+				'data_types' => array()
+			));
+			
+			if(is_null($args['author']) && Auth::is_signed_in()) {
+				$args['author'] = Auth::get_name();
+			}
+			
+			$spreadsheet = new PhpSpreadsheet\Spreadsheet();
+			
+			$spreadsheet->getProperties()
+				->setCreator($args['author'])
+				->setLastModifiedBy($args['author'])
+				->setTitle($args['title']);
+			
+			$sheet = $spreadsheet->getActiveSheet();
+			
+			$columns = null;
+			$row = 0;
+			
+			foreach($this->collection as $i => $column_name) {
+				$sheet->setCellValueByColumnAndRow($i + 1, 1, $column_name);
+			}
+			
+			return $spreadsheet;
+		}
+	}
